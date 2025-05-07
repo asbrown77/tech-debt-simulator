1eb56fc5d321b93fb75eab3bbc7f6e5c50ef1cd2
asbrown77 <alexander.brown@bluefinltd.com>
2025-05-07 22:02:57 +0100

    change router of game to techdebtgame

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/techdebt-simulator/play">
+    <BrowserRouter basename="/techdebtgame">
       <App />
     </BrowserRouter>
   </React.StrictMode>
